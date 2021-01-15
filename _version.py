7c4ece17a7dd2b1250092012a6bb70794b60f6b6
Neizvestnyj <pikromat1995@gmail.com>
2021-01-15 23:36:21 +0300

    Add files via upload

---
+++ _version.py
@@ -0,0 +1,5 @@
+# THIS FILE IS GENERATED FROM KIVYMD SETUP.PY
+__version__ = "0.104.2.dev0"
+__hash__ = "a9c61fce4206c9352ddff432d89c66f0f3c0d81c"
+__short_hash__ = "a9c61fc"
+__date__ = "2021-01-15"
