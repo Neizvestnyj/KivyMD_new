c76981cddd9f6b1599b601565386b0631cbda17e
Neizvestnyj <pikromat1995@gmail.com>
2021-01-16 00:29:47 +0300

    Update dialog.py

--- kivymd/uix/dialog.py
+++ kivymd/uix/dialog.py
@@ -64,12 +64,10 @@ Usage
 
 __all__ = ("MDDialog",)
 
-from kivy.clock import Clock
 from kivy.core.window import Window
 from kivy.lang import Builder
 from kivy.metrics import dp
 from kivy.properties import (
-    ColorProperty,
     ListProperty,
     NumericProperty,
     ObjectProperty,
@@ -96,14 +94,13 @@ Builder.load_string(
         RoundedRectangle:
             pos: self.pos
             size: self.size
-            radius: root.radius
+            radius: [5]
         Scale:
             origin: self.center
             x: root._scale_x
             y: root._scale_y
     canvas.after:
         PopMatrix
-
 
 <MDDialog>
 
@@ -118,9 +115,7 @@ Builder.load_string(
 
         canvas:
             Color:
-                rgba:
-                    root.theme_cls.bg_dark \
-                    if not root.md_bg_color else root.md_bg_color
+                rgba: root.theme_cls.bg_dark
             RoundedRectangle:
                 pos: self.pos
                 size: self.size
@@ -181,24 +176,6 @@ Builder.load_string(
 
 
 class BaseDialog(ThemableBehavior, ModalView):
-    radius = ListProperty([7, 7, 7, 7])
-    """
-    Dialog corners rounding value.
-
-    .. code-block:: python
-
-        self.dialog = MDDialog(
-            text="Oops! Something seems to have gone wrong!",
-            radius=[20, 7, 20, 7],
-        )
-
-    .. image:: https://github.com/HeaTTheatR/KivyMD-data/raw/master/gallery/kivymddoc/dialog-radius.png
-        :align: center
-
-    :attr:`radius` is an :class:`~kivy.properties.ListProperty`
-    and defaults to `[7, 7, 7, 7]`.
-    """
-
     _scale_x = NumericProperty(1)
     _scale_y = NumericProperty(1)
 
@@ -255,6 +232,24 @@ class MDDialog(BaseDialog):
     and defaults to `''`.
     """
 
+    radius = ListProperty([7, 7, 7, 7])
+    """
+    Dialog corners rounding value.
+
+    .. code-block:: python
+
+        self.dialog = MDDialog(
+            text="Oops! Something seems to have gone wrong!",
+            radius=[20, 7, 20, 7],
+        )
+
+    .. image:: https://github.com/HeaTTheatR/KivyMD-data/raw/master/gallery/kivymddoc/dialog-radius.png
+        :align: center
+
+    :attr:`radius` is an :class:`~kivy.properties.ListProperty`
+    and defaults to `[7, 7, 7, 7]`.
+    """
+
     buttons = ListProperty()
     """
     List of button objects for dialog.
@@ -282,7 +277,7 @@ class MDDialog(BaseDialog):
     Objects must be inherited from :class:`~kivymd.uix.list.BaseListItem` class.
 
     With type 'simple'
-    ~~~~~~~~~~~~~~~~~~
+    -----------------
 
     .. code-block:: python
 
@@ -340,7 +335,7 @@ class MDDialog(BaseDialog):
         :align: center
 
     With type 'confirmation'
-    ~~~~~~~~~~~~~~~~~~~~~~~~
+    -----------------------
 
     .. code-block:: python
 
@@ -425,14 +420,6 @@ class MDDialog(BaseDialog):
     and defaults to `[]`.
     """
 
-    width_offset = NumericProperty(dp(48))
-    """
-    Dialog offset from device width.
-
-    :attr:`width_offset` is an :class:`~kivy.properties.NumericProperty`
-    and defaults to `dp(48)`.
-    """
-
     type = OptionProperty(
         "alert", options=["alert", "simple", "confirmation", "custom"]
     )
@@ -448,7 +435,7 @@ class MDDialog(BaseDialog):
     """
     Custom content class.
 
-    .. code-block:: python
+    .. code-block::
 
         from kivy.lang import Builder
         from kivy.uix.boxlayout import BoxLayout
@@ -517,33 +504,18 @@ class MDDialog(BaseDialog):
     and defaults to `'None'`.
     """
 
-    md_bg_color = ColorProperty(None)
-    """
-    Background color in the format (r, g, b, a).
-
-    :attr:`md_bg_color` is an :class:`~kivy.properties.ColorProperty`
-    and defaults to `None`.
-    """
-
     _scroll_height = NumericProperty("28dp")
     _spacer_top = NumericProperty("24dp")
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        Window.bind(on_resize=self.update_width)
 
-        self.md_bg_color = (
-            self.theme_cls.bg_dark if not self.md_bg_color else self.md_bg_color
-        )
-
-        if self.size_hint == [1, 1] and (
-            DEVICE_TYPE == "desktop" or DEVICE_TYPE == "tablet"
-        ):
+        if self.size_hint == [1, 1] and DEVICE_TYPE == "mobile":
             self.size_hint = (None, None)
-            self.width = min(dp(560), Window.width - self.width_offset)
-        elif self.size_hint == [1, 1] and DEVICE_TYPE == "mobile":
+            self.width = dp(280)
+        elif self.size_hint == [1, 1] and DEVICE_TYPE == "desktop":
             self.size_hint = (None, None)
-            self.width = min(dp(280), Window.width - self.width_offset)
+            self.width = dp(560)
 
         if not self.title:
             self._spacer_top = 0
@@ -553,7 +525,6 @@ class MDDialog(BaseDialog):
         else:
             self.create_buttons()
 
-        update_height = False
         if self.type in ("simple", "confirmation"):
             if self.type == "confirmation":
                 self.ids.spacer_top_box.add_widget(MDSeparator())
@@ -564,25 +535,10 @@ class MDDialog(BaseDialog):
                 self.ids.container.remove_widget(self.ids.scroll)
                 self.ids.container.remove_widget(self.ids.text)
                 self.ids.spacer_top_box.add_widget(self.content_cls)
+                self._spacer_top = self.content_cls.height + dp(24)
                 self.ids.spacer_top_box.padding = (0, "24dp", "16dp", 0)
-                update_height = True
         if self.type == "alert":
             self.ids.scroll.bar_width = 0
-
-        if update_height:
-            Clock.schedule_once(self.update_height)
-
-    def update_width(self, *args):
-        self.width = max(
-            self.height + self.width_offset,
-            min(
-                dp(560) if DEVICE_TYPE != "mobile" else dp(280),
-                Window.width - self.width_offset,
-            ),
-        )
-
-    def update_height(self, *_):
-        self._spacer_top = self.content_cls.height + dp(24)
 
     def on_open(self):
         # TODO: Add scrolling text.
@@ -616,7 +572,7 @@ class MDDialog(BaseDialog):
                 self.ids.box_items.add_widget(item)
 
         if height > Window.height:
-            self.set_normal_height()
+            # self.set_normal_height()
             self.ids.scroll.height = self.get_normal_height()
         else:
             self.ids.scroll.height = height
